186cfce23051d43f8de3d330d4c587155aba7aa1
ragil333 <ragilwilujeng333@gmail.com>
2023-10-14 12:32:36 +0700

    change env

--- config/Database.go
+++ config/Database.go
@@ -15,7 +15,8 @@ func DbConfig() string {
 	db_name := os.Getenv("DB_NAME")
 	db_host := os.Getenv("DB_HOST")
 	db_user := os.Getenv("DB_USER")
+	db_port := os.Getenv("DB_PORT")
 	db_password := os.Getenv("DB_PASSWORD")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", db_user, db_password, db_host, db_name)
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true", db_user, db_password, db_host, db_port, db_name)
 	return dsn
 }
